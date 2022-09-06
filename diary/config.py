196deeb12325d14e0f2c2a87badc9d1928322344
이민석 <dat0802@gmail.com>
2022-09-06 12:17:26 +0900

    :sparkles: (develop) 프로젝트 생성

---
+++ diary/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # .env 파일을 읽어서 환경변수에 넣어줌
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DIARY_DATABASE_NAME'],
+        'USER': os.environ['DIARY_DATABASE_USER'],
+        'PASSWORD': os.environ['DIARY_DATABASE_PASSWORD'],
+        'HOST': os.environ['DIARY_DATABASE_HOST'],
+        'PORT': int(os.environ.get('DIARY_DATABASE_PORT', '3306')),
+        'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+SECRET_KEY = os.environ['DIARY_SECRET_KEY']

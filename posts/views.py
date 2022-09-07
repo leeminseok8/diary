5b9ca9c48511bf684d1480b1b62226b3e1132cdd
이민석 <dat0802@gmail.com>
2022-09-07 17:36:26 +0900

    (feature/posts) 일기장 views.py 작성

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .permissions import PostPermission
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    """
+    자유 게시판, 댓글 기능 포함
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [PostPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(author_id=self.request.user.id)
+        return super().perform_create(serializer)

d471c38d7646f47fff2c35756ee902070c46b2ec
이민석 <dat0802@gmail.com>
2022-09-30 21:43:05 +0900

    Feat: CRUD 시리얼라이저 생성 및 비밀번호 검증 및 암호화 구현

---
+++ posts/serializers.py
@@ -0,0 +1,34 @@
+import re
+
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import serializers
+
+from posts.models import Post
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "content",
+            "password",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate_password(self, password):
+        if len(password) < 6:
+            raise serializers.ValidationError("비밀번호는 최소 6자 이상 입력하세요.")
+        elif re.search("[0-9]+", password) is None:
+            raise serializers.ValidationError("비밀번호는 최소 1개 이상의 숫자가 포함되어야 합니다.")
+        return self.password_hash(password)
+
+    def password_hash(self, password):
+        if password:
+            re_password = make_password(password)
+        return re_password

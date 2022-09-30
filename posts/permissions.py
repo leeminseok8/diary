d23a6e07696a9ed0635986c78dc2d42e89f86025
이민석 <dat0802@gmail.com>
2022-09-30 21:42:12 +0900

    Feat: http method에 따른 권한 생성

---
+++ posts/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth.hashers import check_password
+
+
+class IsAuthorUser(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """
+        죄회 : 모든 유저 권한 부여
+        생성, 수정, 삭제 : 비밀번호
+        """
+
+        if request.method in SAFE_METHODS:
+            return True
+        elif request.method == "POST":
+            return True
+        elif request.method in ("PATCH", "PUT", "DELETE"):
+            return check_password(request.data["password"], obj.password)
